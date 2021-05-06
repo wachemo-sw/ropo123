00c669aae8689591e8f2c797f31589f314fe5022
user <user@192.168.137.45>
2021-05-06 05:40:47 +0100

    kjhk

--- wachemosw/src/wachemosw/dd.java
+++ wachemosw/src/wachemosw/dd.java
@@ -3,7 +3,8 @@ package wachemosw;
 public class dd {
 	public static void main(String args[]){
 		System.out.println("bbvvbvbvbvb");
+		System.out.println("bbvvbvbvbvb");
 	
 }
-	//rtyrtyrtrtyrtrtyryryf
-}ugu
+	
+}
